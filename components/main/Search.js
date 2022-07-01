54e9b940e9e1dfc0d42e25fb0d154e299cb701db
Sanjay Kukreti <sanjaykukreti900@gmail.com>
2022-07-01 22:58:31 +0530

    completed

--- components/main/Search.js
+++ components/main/Search.js
@@ -13,20 +13,44 @@ require("firebase/firestore");
 
 export default function Search(props) {
   const [users, setUsers] = useState([]);
-  const fetchUsers = (search) => {
-    firebase
-      .firestore()
-      .collection("users")
-      .where("name", ">=", search)
-      .get()
-      .then((snapshot) => {
-        let users = snapshot.docs.map((doc) => {
-          const data = doc.data();
-          const id = doc.id;
-          return { id, ...data };
+  const fetchUsers = (search, isTrue) => {
+    if (isTrue == false) {
+      firebase
+        .firestore()
+        .collection("users")
+        .where("name", ">=", search)
+        .get()
+        .then((snapshot) => {
+          let users = snapshot.docs.map((doc) => {
+            const data = doc.data();
+            const id = doc.id;
+            return { id, ...data };
+          });
+          setUsers(users);
         });
-        setUsers(users);
-      });
+    } else {
+      let a = [];
+      firebase
+        .firestore()
+        .collection("users")
+        .get()
+        .then((snapshot) => {
+          let users = snapshot.docs.map((doc) => {
+            const data = doc.data();
+            const id = doc.id;
+            return { id, ...data };
+          });
+          console.log(users + " ................................." + search);
+          for (let i = 0; i < users.length; i++) {
+            if (users[i].keyword != undefined) {
+              if (users[i].keyword.indexOf(search) != -1) {
+                a.push(users[i]);
+              }
+            }
+          }
+          setUsers(a);
+        });
+    }
   };
   return (
     <View style={{ height: 1000, backgroundColor: "white" }}>
@@ -47,8 +71,28 @@ export default function Search(props) {
           borderWidth: 0.7,
         }}
         placeholder="Type Here....."
-        onChangeText={(search) => fetchUsers(search)}
+        onChangeText={(search) => fetchUsers(search, false)}
       />
+
+      <TextInput
+        style={{
+          margin: 30,
+
+          width: 300,
+          textAlign: "center",
+          height: 40,
+          backgroundColor: "white",
+          color: "black",
+          elevation: 40,
+          shadowColor: "#87ceeb",
+          borderColor: "black",
+          borderRadius: 0,
+          borderWidth: 0.7,
+        }}
+        placeholder="Type Here Skills....."
+        onChangeText={(search) => fetchUsers(search, true)}
+      />
+
       <FlatList
         style={{ height: 1000 }}
         numColumns={1}
